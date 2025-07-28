2bc5fad70db86caef008448519875d53d5e152bd
KushagraChandra1807 <kushagrachandra1807@gmail.com>
2025-07-28 19:22:43 +0530

    � Updated styles and fixed Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/gtavlandingpage/', // ✅ GitHub repo name here
+  base: '/gtavlandingpage/', // 💥 Use your actual GitHub repo name here
   plugins: [react(), tailwindcss()],
 })
