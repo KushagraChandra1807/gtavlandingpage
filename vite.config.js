149d2bc1e212acc81dcbe9910849b4fd7c47733c
KushagraChandra1807 <kushagrachandra1807@gmail.com>
2025-07-28 19:18:39 +0530

    Added pricedown font and updated index.css

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/gtavlandingpage/', // ✅ GitHub repo name here
+  plugins: [react(), tailwindcss()],
 })
